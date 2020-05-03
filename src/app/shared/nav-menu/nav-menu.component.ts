f3466d4edd00ef11c07eb08fc5f4fd463019a191
sr-shaheen <shaheenfgs@gmail.com>
2020-05-03 18:12:40 +0600

    same changes

--- src/app/shared/nav-menu/nav-menu.component.ts
+++ src/app/shared/nav-menu/nav-menu.component.ts
@@ -1,14 +1,8 @@
-import { MediaMatcher } from '@angular/cdk/layout';
 import {
-  ChangeDetectorRef,
   Component,
   OnDestroy,
   OnInit,
-  ViewChild,
-  AfterViewInit,
-  Input,
 } from '@angular/core';
-import { Observable, Subscription, of } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CommonService } from '../services/common.service';
 import { AsyncService } from '../services/async.service';
@@ -32,33 +26,7 @@ export class NavMenuComponent implements OnInit, OnDestroy {
     this.accountService.user.subscribe((x) => (this.user = x));
   }
 
-  dashboard() {
-    this.router.navigate(['/dashboard']);
-    this.link=this.router.url
-    console.log(this.link);
-
-  }
-  orders() {
-    this.router.navigate(['/orders']);
-    this.link=this.router.url
-    console.log(this.link);
-
-  }
-  customer() {
-    this.router.navigate(['/customer']);
-    this.link=this.router.url
-    console.log(this.link);
-
-  }
-  maps() {
-    this.router.navigate(['/maps']);
-    this.link=this.router.url
-    console.log(this.link);
-
-  }
   ngOnInit(): void {
-    console.log(this.router.url,'oninit');
-
   }
 
   logout() {
