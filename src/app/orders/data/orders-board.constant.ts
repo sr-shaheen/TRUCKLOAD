39ccc95be6386ee9256868f0c64c55578101bfc9
sr-shaheen <shaheenfgs@gmail.com>
2020-05-05 12:15:14 +0600

    board top design done

--- src/app/orders/data/orders-board.constant.ts
+++ src/app/orders/data/orders-board.constant.ts
@@ -9,21 +9,21 @@ export const statusList: string[] = [
 ];
 
 export const statusDictionary = {
-  'ordersPlaced': 'First Day',
-  'detailsCollected': 'Third Day',
-  'orderConfirmed': 'Complaint' ,
-  'loadCompleted': 'In Progress',
-  'inTransit': 'Resolation',
-  'uploadComplete': 'Resolation',
-  'consignmentDone': 'Resolation'
+  'ordersPlaced': 'Orders Placed',
+  'detailsCollected': 'Details Collected',
+  'orderConfirmed': 'Order Confirmed' ,
+  'loadCompleted': 'Load Completed',
+  'inTransit': 'In Transit',
+  'uploadComplete': 'Upload Complete',
+  'consignmentDone': 'Consignment Done'
 };
 
 export const ordersBoardColors = {
-  ordersPlaced: {bgColor: 'rgb(196, 23, 23)', color: '#0e0e0e'},
-  detailsCollected: {bgColor: 'rgba(255, 193, 7, 0.75)', color: '#0e0e0e'},
-  orderConfirmed: {bgColor: '#eeff41', color: '#0e0e0e'},
-  loadCompleted: {bgColor: '#8bc34a', color: '#0e0e0e'},
-  inTransit: {bgColor: 'rgb(124,252,0)', color: '#0e0e0e'},
-  uploadComplete: {bgColor: 'rgb(0,255,0)', color: '#0e0e0e'},
-  consignmentDone: {bgColor: '#rgb(0,128,0)', color: '#0e0e0e'}
+  ordersPlaced: {bgColor: 'rgb(196, 23, 23)', color: '#FFFFFF'},
+  detailsCollected: {bgColor: 'rgba(255, 193, 7, 0.75)', color: '#FFFFFF'},
+  orderConfirmed: {bgColor: '#eeff41', color: '#FFFFFF'},
+  loadCompleted: {bgColor: '#E9DB0F', color: '#FFFFFF'},
+  inTransit: {bgColor: 'rgb(124,252,0)', color: '#FFFFFF'},
+  uploadComplete: {bgColor: 'rgb(0,255,0)', color: '#FFFFFF'},
+  consignmentDone: {bgColor: 'rgb(0,128,0)', color: '#FFFFFF'}
 };
--- src/app/orders/orders-board/orders-board.component.ts
+++ src/app/orders/orders-board/orders-board.component.ts
@@ -4,12 +4,34 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+
+import {
+  statusDictionary,
+  ordersBoardColors,
+  statusList,
+} from '../data/orders-board.constant';
+import { OrdersBoardItem } from '../models/orders-board-item.model';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-orders-board',
   templateUrl: './orders-board.component.html',
   styleUrls: ['./orders-board.component.scss'],
 })
 export class OrdersBoardComponent implements OnInit {
+  ordersPlaced: OrdersBoardItem[] = [];
+  detailsCollected: OrdersBoardItem[] = [];
+  orderConfirmed: OrdersBoardItem[] = [];
+  loadCompleted: OrdersBoardItem[] = [];
+  inTransit: OrdersBoardItem[] = [];
+  uploadComplete: OrdersBoardItem[] = [];
+  consignmentDone: OrdersBoardItem[] = [];
+
+  colors = ordersBoardColors;
+
+  status = statusDictionary;
+  boardData: OrdersBoardItem;
+
+  ordersBoardSub: Subscription;
   constructor() {}
 
   ngOnInit(): void {}
