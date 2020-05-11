fccdf4bb64121a40a3e0cd3076a6604a684864e6
sr-shaheen <shaheenfgs@gmail.com>
2020-05-11 12:59:51 +0600

    orders board modal component created

---
+++ src/app/orders/details-collected-modal/details-collected-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-collected-modal',
+  templateUrl: './details-collected-modal.component.html',
+  styleUrls: ['./details-collected-modal.component.scss']
+})
+export class DetailsCollectedModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/orders/order-confirmed-modal/order-confirmed-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-confirmed-modal',
+  templateUrl: './order-confirmed-modal.component.html',
+  styleUrls: ['./order-confirmed-modal.component.scss']
+})
+export class OrderConfirmedModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
